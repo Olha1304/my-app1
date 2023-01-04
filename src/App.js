1211c4c8fde441fc836c545fec32c807edf4e0e6
Olha1304 <olhakolisnyk1@icloud.com>
2023-01-04 19:58:12 +0200

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <footer>
+        This project made by Olha Kolisnyk with open-sourced GitHub and hosted
+        by Netlify
+      </footer>
     </div>
   );
 }
